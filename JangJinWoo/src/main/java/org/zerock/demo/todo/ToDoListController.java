1976cde47a788b8b8d4c8e648197faf03946685b
Jinwoo Chang <jenny010925@naver.com>
2022-09-12 21:05:48 +0900

    first commit

---
+++ JangJinWoo/src/main/java/org/zerock/demo/todo/ToDoListController.java
@@ -0,0 +1,29 @@
+package org.zerock.demo.todo;
+
+import org.zerock.demo.todo.Service.TodoService;
+import org.zerock.demo.todo.dto.TodoDTO;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name="ToDoListController", urlPatterns = "/todo/list")
+public class ToDoListController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws
+            ServletException, IOException {
+
+        System.out.println("/todo/list");
+
+        List<TodoDTO> dtoList = TodoService.INSTANCE.getList();
+
+        req.setAttribute("list",dtoList);
+
+        req.getRequestDispatcher("/WEB-INF/todo/list.jsp").forward(req,resp);
+    }
+}
---
+++ JangJinWoo/src/main/java/org/zerock/demo/todo/ToDoRegisterController.java
@@ -0,0 +1,28 @@
+package org.zerock.demo.todo;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name="ToDoRegisterController", urlPatterns = "/todo/register")
+public class ToDoRegisterController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws
+            ServletException, IOException {
+
+        resp.sendRedirect("/todo/list");
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws
+            ServletException, IOException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/todo/register.jsp");
+
+        dispatcher.forward(req,resp);
+    }
+
+}
